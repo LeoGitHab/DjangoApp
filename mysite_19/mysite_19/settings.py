e50d6862fa0206b78b8dcf7d23c98081b91e1ea8
leo <leo196722@yandex.ru>
2023-12-09 15:27:56 +1000

    prepare to replace static files

--- mysite_19/mysite_19/settings.py
+++ mysite_19/mysite_19/settings.py
@@ -197,6 +197,12 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
